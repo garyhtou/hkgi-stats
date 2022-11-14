2b243d520be5eada5ac543968111fd3a7c995556
Gary Tou <gary@garytou.com>
2022-11-14 11:08:29 -0500

    Batch the batched write commits to db
    kinda stupid, but firebase only let's you write 500 objects at time

--- src/jobs/syncActivity.ts
+++ src/jobs/syncActivity.ts
@@ -13,18 +13,26 @@ export default async function syncActivity(): Promise<IActivity[]> {
 	}));
 
 	// Upload only new activities to Firebase Firestore
-	const batch = db.batch();
 	const activityRef = db.collection('activity');
 	const snapshot = await activityRef.select('id').get();
 	const existingIds = snapshot.docs.map((doc) => doc.data().id) as TId[];
 	const newActivities = activities.filter((a) => !existingIds.includes(a.id));
-	newActivities.forEach((a) => {
-		const docRef = activityRef.doc(a.id);
-		batch.set(docRef, a);
-	});
-	await batch.commit();
 
-	console.log(`Synced ${newActivities.length} new activities`);
+	// Batch the batch write commits... firebase only lets you do 500 at a time
+	const BATCH_SIZE = 500;
+	for (let i = 0; i < newActivities.length; i += BATCH_SIZE) {
+		const batch = db.batch();
+		const batchActivities = newActivities.slice(i, i + BATCH_SIZE);
+		batchActivities.forEach((a) => {
+			const docRef = activityRef.doc(a.id);
+			batch.set(docRef, a);
+		});
+
+		await batch.commit();
+		console.log(`Uploaded ${batchActivities.length} activities (batch)`);
+	}
+
+	console.log(`Synced a total of ${newActivities.length} new activities`);
 	return newActivities;
 }
 
